4abcd3157e8735afe3c905071c410bcebf36fa5b
Morgan Bazalgette <morgan@morganbaz.com>
2021-10-21 10:38:59 +0200

    add column flag "-c COLUMN" (closes #7), bump to 0.2.0

--- cutcsv.c
+++ cutcsv.c
@@ -6,13 +6,14 @@
 #include<stdbool.h>
 #include<stdio.h>
 
-#define VERSION "0.1.0"
+#define VERSION "0.2.0"
 
 /* flag parsing ------------------------------------------------------------- */
 
 struct field_spec {
 	int32_t min;
 	int32_t max;
+	char*   col_name;
 };
 
 struct flag_info {
@@ -43,9 +44,12 @@ parse_print_usage(char* prog, char* reason) {
 	fprintf(stderr, "%s - the csv swiss army knife (version %s)\n\n\
 Usage: %s OPTION... [FILE]\n\
 Print selected CSV fields from each specified FILE to standard output.\n\
+Select one or more fields using -c and -f. At least one field must be selected.\n\
 With no FILE, or when FILE is -, read standard output.\n\
 \n\
-	-f <LIST>   Print the specified comma-separated list of fields or ranges (required)\n\
+	-f <LIST>   Print the specified comma-separated list of fields or ranges\n\
+	-c <COLUMN> Select fields whose column name (value of the first line)\n\
+	            matches COLUMN.\n\
 	-d <CHAR>   Use CHAR as a delimiter (defaults to ',')\n\
 	-D <STRING> Use STRING as the output separator (in case of multiple fields).\n\
 	            Defaults to the input delimiter.\n\
@@ -63,7 +67,7 @@ With no FILE, or when FILE is -, read standard output.\n\
 bool
 parse_field_spec(struct flag_info* flags, char* arg) {
 	char c;
-	struct field_spec fs = {0, 0};
+	struct field_spec fs = {0, 0, NULL};
 	bool parsed_one = false;
 
 	while (arg[0] != 0) {
@@ -167,13 +171,22 @@ parse_flags(int32_t argc, char* argv[]) {
 				}
 				advance_arg();
 				break;
+			case 'c':
+				/* column name */
+				if (strlen(flag_arg) == 0) {
+					snprintf(invalid_reason, 200, "-c requires an argument");
+					return parse_print_usage(prog, invalid_reason);
+				}
+				ret->fields = realloc(ret->fields, ++ret->field_count * sizeof(struct field_spec));
+				ret->fields[ret->field_count-1] = (struct field_spec) {-1, -1, flag_arg};
+				advance_arg();
+				break;
 			case 'd':
 				/* delim flag */
 				if (strlen(flag_arg) != 1) {
 					snprintf(invalid_reason, 200, "invalid length for delimiter: need 1 got %ld", strlen(flag_arg));
 					return parse_print_usage(prog, invalid_reason);
 				}
-
 				ret->delim = flag_arg[0];
 				advance_arg();
 				break;
@@ -216,16 +229,52 @@ parse_flags(int32_t argc, char* argv[]) {
 	return ret;
 }
 
+/* column matching helper functions ----------------------------------------- */
+
+bool
+check_column_match(char* fieldbuf, int32_t fieldbuf_pos, struct flag_info* flags, int32_t field_num) {
+	int i;
+	struct field_spec fs;
+
+	for (i = 0; i < flags->field_count; i++) {
+		fs = flags->fields[i];
+		if (fs.col_name == NULL)
+			continue;
+		if (strncmp(fieldbuf, fs.col_name, fieldbuf_pos) == 0) {
+			/* we add a new fieldspec for each field_num that matches. while this
+			 * may seem inefficient, it is simple and it allows us to reset the "extra"
+			 * fields when changing files. different files may have different ordering
+			 * of columns, and we should respect that. */
+			flags->fields = realloc(flags->fields, ++flags->field_count * sizeof(struct field_spec));
+			flags->fields[flags->field_count-1] = (struct field_spec) {field_num, field_num, fs.col_name};
+			return true;
+		}
+	}
+	return false;
+}
+
+void
+reset_extra_field_specs(struct flag_info* flags) {
+	int32_t i;
+
+	for (i = 0; i < flags->field_count; i++) {
+		if (flags->fields[i].col_name != NULL && flags->fields[i].min >= 0) {
+			/* this is the first extra field. delete from here. */
+			flags->field_count = i;
+		}
+	}
+}
+
 /* main program ------------------------------------------------------------- */
 
 /* determine whether a field should be printed based on the flags. */
 bool
-should_print_field(struct flag_info* flags, int32_t num) {
+should_print_field(struct flag_info* flags, int32_t num, int32_t row_num) {
 	int32_t i;
 	struct field_spec curr;
 
 	// Still have rows to skip
-	if (flags->skip_rows > 0)
+	if (row_num <= flags->skip_rows)
 		return false;
 
 	for (i = 0; i < flags->field_count; i++) {
@@ -250,11 +299,12 @@ main(int32_t argc, char* argv[]) {
 	size_t read;
 	int32_t pos;
 	int32_t field_num;
+	int32_t row_num;
 	bool in_quotes;
 	char last_char = 0;
-	char* to_print;
-	size_t to_print_sz = BUF_SIZE;
-	int32_t to_print_pos = 0;
+	char* fieldbuf;
+	size_t fieldbuf_sz = BUF_SIZE;
+	int32_t fieldbuf_pos = 0;
 	bool should_print;
 	int32_t num_printed;
 
@@ -271,15 +321,22 @@ main(int32_t argc, char* argv[]) {
 
 	/* allocate buffers */
 	buf = malloc(BUF_SIZE);
-	to_print = calloc(BUF_SIZE, 1);
+	fieldbuf = calloc(BUF_SIZE, 1);
 
 	for (i = 0; i < flags->file_count; i++) {
 		in_quotes = false;
-		should_print = should_print_field(flags, 1);
+		should_print = should_print_field(flags, 1, 1);
 		read = 0;
 		pos = 0;
 		field_num = 1;
+		row_num = 1;
 		num_printed = 0;
+		last_char = 0;
+		if (i > 0) {
+			/* reset fieldspecs coming from matching column names,
+			 * as they will need to be rematched in this second file. */
+			reset_extra_field_specs(flags);
+		}
 
 		verbose("opening file %s\n", flags->files[i]);
 
@@ -289,13 +346,12 @@ main(int32_t argc, char* argv[]) {
 			return 1;
 		}
 #define addchar(x) \
-	if (!should_print) continue;\
-	if (to_print_pos+1 >= to_print_sz) {\
-		to_print = realloc(to_print, to_print_sz * 2);\
-		memset(&to_print[to_print_sz], 0, to_print_sz);\
-		to_print_sz *= 2;\
+	if (fieldbuf_pos+1 >= fieldbuf_sz) {\
+		fieldbuf = realloc(fieldbuf, fieldbuf_sz * 2);\
+		memset(&fieldbuf[fieldbuf_sz], 0, fieldbuf_sz);\
+		fieldbuf_sz *= 2;\
 	} \
-	to_print[to_print_pos++] = x;\
+	fieldbuf[fieldbuf_pos++] = x;\
 	last_char = x
 		for (read = fread(buf, 1, BUF_SIZE, file); read != 0; read = fread(buf, 1, BUF_SIZE, file)) {
 			for (pos = 0; pos < read; pos++) {
@@ -331,37 +387,42 @@ main(int32_t argc, char* argv[]) {
 				}
 				/* \n or delim, in any case we are changing field.
 				 * dump current field if we should print it */
+				if (row_num == 1) {
+					/* check whether fieldbuf == one of the field names in flags.
+					 * if so, recalculate should_print as that might have changed */
+					if (check_column_match(fieldbuf, fieldbuf_pos, flags, field_num))
+						should_print = should_print_field(flags, field_num, row_num);
+				}
 				if (should_print) {
 					if (num_printed > 0)
 						fputs(flags->out_delim, stdout);
-					fwrite(to_print, sizeof(char), to_print_pos, stdout);
-					to_print_pos = 0;
+					fwrite(fieldbuf, sizeof(char), fieldbuf_pos, stdout);
 					num_printed++;
 				}
+				fieldbuf_pos = 0;
 				if (buf[pos] == '\n') {
 					if (num_printed > 0)
 						fputc('\n', stdout);
 					field_num = 1;
 					num_printed = 0;
-					if (flags->skip_rows > 0)
-						flags->skip_rows--;
+					row_num++;
 				} else {
 					field_num++;
 				}
 				last_char = 0;
-				should_print = should_print_field(flags, field_num);
+				should_print = should_print_field(flags, field_num, row_num);
 			}
 		}
 
 		/* print last one in case we are missing a newline at the end of the
-		 * file. notice we check to_print_pos to account for empty lines (last line) */
-		if (should_print && to_print_pos != 0) {
+		 * file. notice we check fieldbuf_pos to account for empty lines (last line) */
+		if (should_print && fieldbuf_pos != 0) {
 			if (num_printed > 0)
 				fputs(flags->out_delim, stdout);
-			fwrite(to_print, sizeof(char), to_print_pos, stdout);
-			to_print_pos = 0;
+			fwrite(fieldbuf, sizeof(char), fieldbuf_pos, stdout);
 			num_printed++;
 		}
+		fieldbuf_pos = 0;
 
 		if (num_printed != 0)
 			fputc('\n', stdout);
--- test.py
+++ test.py
@@ -17,25 +17,47 @@ test_cases = {
     "multiple":         [["-f1-2,4,6-"], b"1,2,3,4,5,6", b"1,2,4,6\n"],
     "until":            [["-f-3"], b"1,2,3,4,5,6", b"1,2,3\n"],
     "from":             [["-f3-"], b"1,2,3,4,5,6", b"3,4,5,6\n"],
+    "colr":             [["-rcname"], b"a,b,name,c\na,b,adolfo,c\n", b"adolfo\n"],
+    "col":              [["-cname"], b"a,b,name,c\na,b,adolfo,c\n", b"name\nadolfo\n"],
+    "col_field":        [["-f1", "-ca", "-cb"], b"1,2,3,a,b,c\n", b"1,a,b\n"],
 }
 
 exit_code = 0
 
-for k in test_cases.keys():
-    v = test_cases[k]
-    args = ["./cutcsv"]
-    args.extend(v[0])
-    call = subprocess.run(args, input=v[1], capture_output=True)
-    k += ":"
-    if call.returncode != 0:
-        print("{0: <16} fail (return code: {1})".format(k, call.returncode))
-        exit_code = 1
-        continue
-    if call.stdout != v[2]:
-        print("{0: <16} fail (wrong return output)".format(k))
-        exit_code = 1
-        continue
-    print("{0: <16} success".format(k))
+def file_put(filename, data):
+    with open(filename, 'wb') as f:
+        f.write(data)
+
+def run_tests(is2x):
+    for k in test_cases.keys():
+        v = test_cases[k]
+        args = ["./cutcsv"]
+        args.extend(v[0])
+        if is2x:
+            file_put('/tmp/cutcsv-a', v[1])
+            file_put('/tmp/cutcsv-b', v[1])
+            args.extend(['/tmp/cutcsv-a', '/tmp/cutcsv-b'])
+        call = subprocess.run(args, input=None if is2x else v[1], capture_output=True)
+        k += ":"
+        if call.returncode != 0:
+            print("{0: <16} fail (return code: {1})".format(k, call.returncode))
+            exit_code = 1
+            continue
+        output_want = (v[2] * 2) if is2x else v[2]
+        if call.stdout != output_want:
+            print("{0: <16} fail (wrong return output)".format(k))
+            print(f"wanted: {output_want}\ngot:    {call.stdout}")
+            exit_code = 1
+            continue
+        print("{0: <16} success".format(k))
+
+run_tests(False)
+
+print("""running 2x tests...
+(these make sure that the state-clearing logic works correctly,
+and given two identical files the output is simply duplicated)""")
+
+run_tests(True)
 
 if exit_code == 0:
     print("everything is green!")
