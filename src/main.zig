9205b543ba4d660b3da151bce91bc21ae54724cf
Morgan Bazalgette <morgan@morganbaz.com>
2025-11-11 00:47:07 +0100

    more optimizations

--- src/main.zig
+++ src/main.zig
@@ -33,17 +33,24 @@ pub fn main() !u8 {
 }
 
 const readIter = struct {
-    buf: [2048]u8 = undefined,
+    buf: [4096]u8 = undefined,
     pos: u16 = 0,
     limit: u16 = 0,
+    err: ?std.fs.File.ReadError = null,
     file: std.fs.File,
-    fn next(self: *readIter) std.fs.File.ReadError!?u8 {
+    fn next(self: *readIter) ?u8 {
         // TODO: utf8 support
 
         // Refill buffer if needed
         if (self.pos >= self.limit) {
             @branchHint(.unlikely);
-            const sz = try self.file.read(&self.buf);
+            if (self.err != null) {
+                return null;
+            }
+            const sz = self.file.read(&self.buf) catch |err| {
+                self.err = err;
+                return null;
+            };
             if (sz == 0) return null; // EOF
 
             self.pos = 0;
@@ -66,23 +73,37 @@ const stateType = struct {
         Field,
         ExpectQuoteDelim,
     } = .ExpectField,
-    fieldBuf: std.ArrayList(u8),
+    fieldBuf: []u8,
+    fieldBufCap: usize,
+
     fieldBitset: std.DynamicBitSetUnmanaged,
     colNum: u64 = 1,
     rowNum: u64 = 1,
     printedInRow: bool = false,
 
+    fn fieldAppend(self: *stateType, gpa: std.mem.Allocator, b: u8) !void {
+        if (self.fieldBuf.len == self.fieldBufCap) {
+            @branchHint(.unlikely);
+            const newCap = self.fieldBufCap + 1024;
+            self.fieldBuf = (try gpa.realloc(self.fieldBuf, newCap))[0..self.fieldBufCap];
+            self.fieldBufCap = newCap;
+        }
+        self.fieldBuf.len += 1;
+        self.fieldBuf[self.fieldBuf.len - 1] = b;
+    }
+
     fn deinit(self: *stateType, gpa: std.mem.Allocator) void {
-        self.fieldBuf.deinit(gpa);
+        gpa.free(self.fieldBuf);
         self.fieldBitset.deinit(gpa);
     }
 
-    fn printField(state: *stateType, gpa: std.mem.Allocator, fl: flags.Flags, sw: *std.io.Writer, isNewline: bool) !void {
+    inline fn printField(state: *stateType, gpa: std.mem.Allocator, fl: flags.Flags, sw: *std.io.Writer, isNewline: bool) !void {
         // \n or delim, in any case we are changing field.
         const bitPos: usize = state.colNum - 1;
+
         if (bitPos >= state.fieldBitset.bit_length) {
             try state.fieldBitset.resize(gpa, bitPos + 1, for (fl.fields.items) |spec| {
-                if ((state.rowNum == 1 and spec == .column and std.mem.eql(u8, spec.column, state.fieldBuf.items)) or
+                if ((state.rowNum == 1 and spec == .column and std.mem.eql(u8, spec.column, state.fieldBuf)) or
                     (spec == .range and
                         (spec.range.min == null or state.colNum >= spec.range.min.?) and
                         (spec.range.max == null or state.colNum <= spec.range.max.?))) break true;
@@ -97,9 +118,9 @@ const stateType = struct {
             } else {
                 state.printedInRow = true;
             }
-            _ = try sw.write(state.fieldBuf.items);
+            _ = try sw.write(state.fieldBuf);
         }
-        state.fieldBuf.shrinkRetainingCapacity(0);
+        state.fieldBuf.len = 0;
         if (isNewline) {
             if (state.printedInRow)
                 try sw.writeByte('\n');
@@ -121,28 +142,21 @@ fn executeFile(gpa: std.mem.Allocator, sw: *std.io.Writer, fl: flags.Flags, file
     defer file.close();
     var ri: readIter = .{ .file = file };
     var state: stateType = .{
-        .fieldBuf = try std.ArrayList(u8).initCapacity(gpa, 1024),
+        .fieldBuf = (try gpa.alloc(u8, 1024))[0..0],
+        .fieldBufCap = 1024,
         .fieldBitset = try std.DynamicBitSetUnmanaged.initEmpty(gpa, 0),
     };
     defer state.deinit(gpa);
 
-    while (try ri.next()) |byte| {
+    while (ri.next()) |byte| {
         switch (state.fsm) {
-            .ExpectField => {
-                if (byte == '"') {
-                    state.fsm = .QuotedField;
-                } else {
-                    try state.fieldBuf.append(gpa, byte);
-                    state.fsm = .Field;
-                }
-            },
             .Field => {
                 if (byte == fl.delim or byte == '\n') {
                     try state.printField(gpa, fl, sw, byte == '\n');
                     state.fsm = .ExpectField;
                 } else {
                     @branchHint(.likely);
-                    try state.fieldBuf.append(gpa, byte);
+                    try state.fieldAppend(gpa, byte);
                 }
             },
             .QuotedField => {
@@ -150,12 +164,20 @@ fn executeFile(gpa: std.mem.Allocator, sw: *std.io.Writer, fl: flags.Flags, file
                     state.fsm = .ExpectQuoteDelim;
                 } else {
                     @branchHint(.likely);
-                    try state.fieldBuf.append(gpa, byte);
+                    try state.fieldAppend(gpa, byte);
+                }
+            },
+            .ExpectField => {
+                if (byte == '"') {
+                    state.fsm = .QuotedField;
+                } else {
+                    try state.fieldAppend(gpa, byte);
+                    state.fsm = .Field;
                 }
             },
             .ExpectQuoteDelim => {
                 if (byte == '"') {
-                    try state.fieldBuf.append(gpa, '"');
+                    try state.fieldAppend(gpa, '"');
                     state.fsm = .QuotedField;
                 } else if (byte == fl.delim or byte == '\n') {
                     try state.printField(gpa, fl, sw, byte == '\n');
@@ -164,10 +186,12 @@ fn executeFile(gpa: std.mem.Allocator, sw: *std.io.Writer, fl: flags.Flags, file
             },
         }
     }
+    if (ri.err != null)
+        return ri.err.?;
 
     // There's some data left-over without a trailing newline, try to see if
     // we should print it.
-    if (state.colNum > 1 or state.fieldBuf.items.len > 0) {
+    if (state.colNum > 1 or state.fieldBuf.len > 0) {
         try state.printField(gpa, fl, sw, true);
     }
 }
